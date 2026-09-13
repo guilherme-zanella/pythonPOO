62b3c0a52be260097455f00562702d801144cb4b
Guilherme Zanella <zanellag722@gmail.com>
2026-09-13 14:57:26 -0300

    Desafio 18 melhorado, assistindo o vídeos

--- desafios/des018/des018.py
+++ desafios/des018/des018.py
@@ -2,25 +2,35 @@ from rich import print
 from rich.panel import Panel
 
 class Churrasco:
+    
+    consumo_pad:float = 0.400
+    custo_pad:float = 82.40
+
     def __init__(self, titulo, quantidade):
         self.titulo = titulo
         self.quantidade = quantidade
 
+    def __str__(self) -> str:
+        return f'Esse é o {self.titulo} com {self.quantidade} convidados'
+
+    def calc_consumo_total(self) -> float:
+        return Churrasco.consumo_pad * self.quantidade
+
+    def calc_custo_total(self) -> float:
+        return self.calc_consumo_total() * Churrasco.custo_pad
+
+    def calc_custo_dividido(self) -> float:
+        return self.calc_custo_total() / self.quantidade
+
     def analisar(self):
-        consumo_pad = 0.4
-        custo_pad = 82.40
-        consumo_tot = consumo_pad * self.quantidade
-        custo_tot = consumo_tot * custo_pad
-        custo_div = custo_tot / self.quantidade
+        conteudo = f'Analisando [green]{self.titulo}[/] com [blue1]{self.quantidade} convidados[/]\n'
+        conteudo += f'Cada participante comerá {Churrasco.consumo_pad}Kg e cada Kg custa R${Churrasco.custo_pad:,.2f}\n'
+        conteudo += f'Recomendo [blue1]comprar {self.calc_consumo_total():.2f}Kg[/] de carne\n'
+        conteudo += f'O custo total será de [green]R${self.calc_custo_total():,.2f}[/]\n'
+        conteudo += f'Cada pessoa pagará [yellow]R${self.calc_custo_dividido():,.2f}[/] para participar.'
 
-        txt_analise = f'Analisando [green]{self.titulo}[/] com [blue1]{self.quantidade} convidados[/]'
-        txt_padrao = f'Cada participante comerá {consumo_pad}Kg e cada Kg custa R${custo_pad:,.2f}'
-        txt_kg = f'Recomendo [blue1]comprar {consumo_tot:.2f}Kg[/] de carne'
-        txt_custo_tot = f'O custo total será de [green]R${custo_tot:,.2f}[/]'
-        txt_custo_div = f'Cada pessoa pagará [yellow]R${custo_div:,.2f}[/] para participar.'
-
-        caixa = Panel(f'{txt_analise}\n{txt_padrao}\n{txt_kg}\n{txt_custo_tot}\n{txt_custo_div}',title=self.titulo, width=70)
-        return caixa
+        painel = Panel(conteudo,title=self.titulo, width=70)
+        return painel
 
 
 c1 = Churrasco('Churras dos Amigos', 15)
