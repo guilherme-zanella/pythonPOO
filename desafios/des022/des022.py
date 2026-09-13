a00774b118fe5294968572962b8d969194792c25
Guilherme Zanella <zanellag722@gmail.com>
2026-09-13 19:59:18 -0300

    Desafio 22 melhorado, assistindo o vídeo

--- desafios/des022/des022.py
+++ desafios/des022/des022.py
@@ -2,51 +2,53 @@ from rich import print
 from rich.panel import Panel
 
 class ControleRemoto:
-    def __init__(self):
-        self.canal = 1
-        self.volume = 2
-        self.ligada = False
+    max_canal = 5
+    min_canal = 1
+    max_volume = 5
+    min_volume = 0
+    def __init__(self, canal=1, volume=2):
+        self.canal:int = canal
+        self.volume:int = volume
+        self.ligada:bool = False   
 
-    def click(self, botao):
-        if botao == '@':
-            if self.ligada:
-                self.ligada = False
-            else:
-                self.ligada = True
-
+    def liga_desliga(self) -> bool:
         if self.ligada:
-            if botao == '-':
-                if self.volume == 0:
-                    pass
-                else:
-                    self.volume -= 1
+            self.ligada = False
+        else:
+            self.ligada = True
 
-            if botao == '+':
-                if self.volume == 5:
-                    pass
-                else:
-                    self.volume += 1
+    def avancar_canal(self) -> int:
+        if self.ligada:
+            if self.canal == ControleRemoto.max_canal:
+                self.canal = ControleRemoto.min_canal
+            else:
+                self.canal += 1
 
-            if botao == '<':
-                if self.canal == 1:
-                    self.canal = 5
-                else:
-                    self.canal -= 1
+    def voltar_canal(self) -> int:
+        if self.ligada:
+            if self.canal == ControleRemoto.min_canal:
+                self.canal = ControleRemoto.max_canal
+            else:
+                self.canal -= 1
 
-            if botao == '>':
-                if self.canal == 5:
-                    self.canal = 1
-                else:
-                    self.canal += 1
+    def aumentar_volume(self) -> int:
+        if self.ligada:
+            if self.volume != ControleRemoto.max_volume:
+                self.volume += 1
+
+    def diminuir_volume(self) -> int:
+        if self.ligada:
+            if self.volume != ControleRemoto.min_volume:
+                self.volume -= 1
 
     def tv(self):
         if self.ligada:
             conteudo = ('Canal  =')
-            for c in range(1, 6):
+            for c in range(ControleRemoto.min_canal, ControleRemoto.max_canal+1):
                 conteudo += f' {c} ' if self.canal != c else f' [white on yellow] {c} [/] '
 
             conteudo += '\nVolume = '
-            for v in range(1, 6):
+            for v in range(ControleRemoto.min_volume+1, ControleRemoto.max_volume+1):
                 conteudo += f'[white on blue1] [/]' if v <= self.volume else f'[white on black] [/]'  
         else:
             conteudo = ':prohibited: [red1]A TV está desligada[/]'
@@ -54,12 +56,23 @@ class ControleRemoto:
         tela = Panel(conteudo, title='[ TV ]', width=30)
         print(tela)
 
+
 controle = ControleRemoto()
 while True:
     controle.tv()
-    botao = input(f'< CH{controle.canal} >  - VOL{controle.volume} + ')
-    if botao == '0':
-        break
-    controle.click(botao)
-    
+    comando =input(f'< CH{controle.canal} >  - VOL{controle.volume} + ')
+    match comando:
+        case '0':
+            break
+        case '@':
+            controle.liga_desliga()
+        case '<':
+            controle.voltar_canal()
+        case '>':
+            controle.avancar_canal()
+        case '-':
+            controle.diminuir_volume()
+        case '+':
+            controle.aumentar_volume()
+    print('\n'*10)
         
