48b69d5635fb26607d4ec8ea7d5fd570ba0464e1
Guilherme Zanella <zanellag722@gmail.com>
2026-09-13 12:01:50 -0300

    Desafio 17 melhorado, assitindo o vídeo

--- desafios/des017/des017.py
+++ desafios/des017/des017.py
@@ -6,12 +6,19 @@ class Produto:
         self.nome = nome
         self.preco = preco
 
+    def __str__(self):
+         return f'{self.nome} custa R${self.preco:,.2f}'
+
     def etiqueta(self):
-        caixa = Panel(f'{self.nome:^30}\n{'—'*30}\n{f' R${self.preco:,.2f} '.center(30,'.')}', title='Produto', width=34)
+        conteudo = f'{self.nome:^30}'
+        conteudo += f'{'—'*30}'
+        conteudo += f'R${self.preco:,.2f} '.center(30, '.')
+
+        caixa = Panel(conteudo, title='Produto', width=34)
         return caixa
 
-p1 = Produto('Iphone 17 Pro Max', 25000.80)
-p2 = Produto('Notebook Games', 8000)
+p1 = Produto('Iphone 17 Pro Max', 25_000.80)
+p2 = Produto('Notebook Games', 8_000)
 
 print(p1.etiqueta())
 print(p2.etiqueta())
