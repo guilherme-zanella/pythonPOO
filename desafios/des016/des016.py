67eeffc40fb91e472c3f87b788610f0e4a059e5d
Guilherme Zanella <zanellag722@gmail.com>
2026-09-13 11:41:12 -0300

    Desafio 16 melhorado, assistindo o vídeo

--- desafios/des016/des016.py
+++ desafios/des016/des016.py
@@ -1,17 +1,25 @@
 from rich import print
+from rich import inspect
 
 class Funcionario:
+    # atributo de classe
+    empresa = 'Curso em Vídeo'
+
     def __init__(self, nome, setor, cargo):
         self.nome = nome
         self.setor = setor
-        self.cargo = cargo
+        self.cargo = cargo  
 
-    def apresentacao(self):
-        return f':handshake: Olá, sou [blue1]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa Curso Em Vídeo'
+    def apresentacao(self) -> str: # indico que a função retorna uma string
+        return f':handshake: Olá, sou [blue1]{self.nome}[/] e sou {self.cargo} do setor de {self.setor} da empresa {Funcionario.empresa}'
 
+
+Funcionario.empresa = 'NETFLIX'
 
 f1 = Funcionario('Maria', 'Administração', 'Diretora')
 f2 = Funcionario('Pedro', 'TI', 'Programador')
 
 print(f1.apresentacao())
 print(f2.apresentacao())
+# inspect(f1, methods=True)
+# inspect(Funcionario)
