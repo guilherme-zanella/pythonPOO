3e6249596074da7f41759d8dbed5eb4b04799046
Guilherme Zanella <zanellag722@gmail.com>
2026-09-15 14:41:07 -0300

    Desafio 23 melhorado, assistindo o vídeo de solução

--- desafios/des023/__main__.py
+++ desafios/des023/__main__.py
@@ -4,13 +4,13 @@ from rich import print
 def main():
     p1 = Quadrado(5)
     print(p1)
-    print(f'Perímetro = {p1.perimetro()}')
-    print(f'Área = {p1.area()}')
+    print(f'Perímetro = {p1.perimetro():.1f}')
+    print(f'Área = {p1.area():.1f}')
 
     p2 = Circulo(20)
     print(p2)
-    print(f'Perímetro = {p2.perimetro()}')
-    print(f'Área = {p2.area()}')
+    print(f'Perímetro = {p2.perimetro():.1f}')
+    print(f'Área = {p2.area():.1f}')
 
 if __name__ == '__main__':
     main()
--- desafios/des023/classes.py
+++ desafios/des023/classes.py
@@ -1,20 +1,21 @@
 from abc import ABC, abstractmethod
+from math import pi
 
 class Poligono(ABC):
     def __init__(self, qtd_lados):
         self.qtd_lados = qtd_lados
 
     @abstractmethod
-    def perimetro(self):
+    def perimetro(self) -> float:
         pass
 
     @abstractmethod
-    def area(self):
+    def area(self) -> float:
         pass
 
 
 class Quadrado(Poligono):
-    def __init__(self, lado):
+    def __init__(self, lado=1):
         super().__init__(qtd_lados=4)
         self.lado = lado
 
@@ -22,22 +23,22 @@ class Quadrado(Poligono):
         return f'Um quadrado de lado {self.lado} tem:'
 
     def perimetro(self):
-        return f'[blue]{self.qtd_lados * self.lado:.1f}[/]'
+        return self.lado * 4
 
     def area(self):
-        return f'[blue]{self.lado ** 2:.1f}[/]'
+        return self.lado ** 2
 
 
 class Circulo(Poligono):
-    def __init__(self, raio):
-        super().__init__(qtd_lados=1)
+    def __init__(self, raio=1):
+        super().__init__(1)
         self.raio = raio
 
     def __str__(self):
         return f'Um círculo de raio {self.raio} tem:'
 
     def perimetro(self):
-        return f'[blue]{self.raio * 2 * 3.14:.1f}[/]'
+        return self.raio * 2 * pi
 
     def area(self):
-        return f'[blue]{self.raio ** 2 * 3.14:.1f}[/]'
+        return self.raio ** 2 * pi
