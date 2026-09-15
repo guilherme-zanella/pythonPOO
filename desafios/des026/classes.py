6d6bbdc38b5224dc3701477aaaf545e163b54497
Guilherme Zanella <zanellag722@gmail.com>
2026-09-15 15:49:20 -0300

    Desafio 26 melhorado, assistindo o vídeo do desafio

--- desafios/des026/classes.py
+++ desafios/des026/classes.py
@@ -5,30 +5,32 @@ from rich import print
 class Funcionario(ABC):
     sal_min = 1612
     inss = 7.5
-    def __init__(self, nome):
+
+    def __init__(self, nome = None):
         self.nome = nome
+        self.sal_bruto = 0
         self.salario = 0
-        self.qtd_sal_min = 0
 
     @abstractmethod
     def calcular_salario(self):
         pass
 
     def analisar_salario(self):
-        conteudo = f'O salário de [blue]{self.nome}[/] ([blue1]Funcionário: {self.__class__.__name__}[/]) é de [green]R${self.salario:.2f}[/] e corresponde a [yellow]{self.qtd_sal_min} salários mínimos[/].'
+        qtd_sal_min = self.salario / Funcionario.sal_min
+
+        conteudo = f'O salário de [blue]{self.nome}[/] ([blue1]Funcionário: {self.__class__.__name__}[/]) é de [green]R${self.salario:.2f}[/] e corresponde a [yellow]{qtd_sal_min:.1f} salários mínimos[/].'
+
         painel = Panel(conteudo, title='Análise de funcionário', width=55)
         print(painel)
 
 
 class Horista(Funcionario):
-    def __init__(self, nome, valor_hora, horas_trab):
+    def __init__(self, nome, valor_hora = 7.37, horas_trab = 220):
         super().__init__(nome)
         self.sal_bruto = valor_hora * horas_trab
 
     def calcular_salario(self):
-        self.salario = self.sal_bruto - (self.sal_bruto * self.inss / 100)
-        self.qtd_sal_min = self.salario / self.sal_min
-        self.qtd_sal_min = round(self.qtd_sal_min, 1)
+        self.salario = self.sal_bruto - (self.sal_bruto * Funcionario.inss / 100)
 
 
 class Mensalista(Funcionario):
@@ -37,7 +39,5 @@ class Mensalista(Funcionario):
         self.sal_bruto = salario
 
     def calcular_salario(self):
-        self.salario = self.sal_bruto - (self.sal_bruto * self.inss / 100)
-        self.qtd_sal_min = self.salario / self.sal_min
-        self.qtd_sal_min = round(self.qtd_sal_min, 1)
+        self.salario = self.sal_bruto - (self.sal_bruto * Funcionario.inss / 100)
     
