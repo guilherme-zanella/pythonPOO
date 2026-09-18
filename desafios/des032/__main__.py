ab7485654647b94ce68c01e829746e0c1cc4c6be
Guilherme Zanella <zanellag722@gmail.com>
2026-09-18 16:40:37 -0300

    Desafio 32 aprimorado, com senha depois de ter aprendido hash

--- desafios/des032/__main__.py
+++ desafios/des032/__main__.py
@@ -2,11 +2,13 @@ from classe import *
 from rich import inspect, print
 
 def main():
-    c = ContaBancaria(22, 'Guilherme', 5000)
-    c.nome = 'guilherme'
+    c = ContaBancaria(22, 'Guilherme', 5000, '123')
+
     c.depositar(100)
-    c.sacar(200)
-    inspect(c, private=True)
+    
+    c.sacar(300)
+    
+    print(c)
 
 
 if __name__ == '__main__':
