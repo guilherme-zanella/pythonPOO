1776676113547213e7e8d945c524a99d0de8117a
Guilherme Zanella <zanellag722@gmail.com>
2026-09-18 17:19:46 -0300

    Desafio 32 melhorado com o input da senha aparecendo *, assistindo o vídeo

--- desafios/des032/__main__.py
+++ desafios/des032/__main__.py
@@ -4,10 +4,15 @@ from rich import inspect, print
 def main():
     c = ContaBancaria(22, 'Guilherme', 5000, '123')
 
+    print('Mudando o nome')
+    c.nome = 'Manuela'
+
+    print('Vou depositar')
     c.depositar(100)
-    
+
+    print('Vou sacar')
     c.sacar(300)
-    
+
     print(c)
 
 
--- desafios/des032/classe.py
+++ desafios/des032/classe.py
@@ -1,58 +1,61 @@
 from hashlib import sha256
 
 class ContaBancaria:
-    def __init__(self, id, nome, saldo=0, chave=None):
+    def __init__(self, id:int, nome:str, saldo:float=0, chave:str=None):
         self._id = id
         self._titular = nome
         self.__saldo = saldo
 
         if not chave:
             chave = self.pede_senha()
-        else:
-            chave = sha256(chave.encode('utf-8')).hexdigest()
-        self.__hash = chave
+        self.__hash = sha256(chave.encode('utf-8')).hexdigest()
 
         print(f'Conta {self._id} criada com sucesso. Saldo atual de R${self.__saldo:,.2f}')
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'A conta {self._id} de {self.nome} tem saldo atual de R${self.__saldo:,.2f}'
 
-    def validar(self, chave):
-        if chave == self.__hash:
+    def validar_senha(self, chave) -> bool:
+        senha = sha256(chave.encode('utf-8')).hexdigest()
+
+        if senha == self.__hash:
             return True
         else:
             return False
 
-    def pede_senha(self):
-        senha = str(input('Senha: '))
-        hash = sha256(senha.encode('utf-8')).hexdigest()
-        return hash
+    def pede_senha(self) -> str:
+        from pwinput import pwinput
+
+        senha = str(pwinput('Senha: ','🔒')).strip()
+        return senha
 
     @property
-    def nome(self):
+    def nome(self) -> str:
         return self._titular
 
     @nome.setter
     def nome(self, nome):
         senha = self.pede_senha()
-        if self.validar(senha):
-            self._titular = nome
-            print(f'Nome da conta {self._id} trocado para {self._titular}')
+
+        if self.validar_senha(senha):
+            if len(nome) > 1:
+                self._titular = nome
+                print(f'Nome da conta {self._id} trocado para {self._titular}')
         else:
             print(f'Senha inválida. Não foi possivel mudar o nome da conta {self._id}')
 
     def sacar(self, valor, chave=None):
         if not chave:
-            hash = self.pede_senha()
-        else:
-            hash = sha256(chave.encode('utf-8')).hexdigest()
+            chave = self.pede_senha()
 
         if valor <= self.__saldo and valor > 0:
-            if self.validar(hash):
+
+            if self.validar_senha(chave):
                 self.__saldo -= valor
                 print(f'Saque de R${valor:.2f}. Autorizado na conta {self._id}')
             else:
                 print(f'Senha inválida! Não foi possível realizar o saque na conta {self._id}')
+
         else:
             raise ValueError('Não pode fazer saques com valores negativos ou maiores que o saldo!')
 
