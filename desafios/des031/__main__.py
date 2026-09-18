d253b4a75f8ac0c65558487874df520ed6faab56
Guilherme Zanella <zanellag722@gmail.com>
2026-09-18 18:13:57 -0300

    Desafio 31 do retangulo, melhorado assistindo o vídeo

--- desafios/des031/__main__.py
+++ desafios/des031/__main__.py
@@ -4,10 +4,13 @@ from rich import print, inspect
 def main():
     r = Retangulo()
 
-    r.base = 10
-    r.altura = 12
+    try:
+        # r.base = 10
+        # r.altura = 12
+        r.medidas = (2,4)
 
-    # r.medidas = (2,4)
+    except Exception as e:
+        print(f'Ocorreu um {type(e).__name__}: {e}')
 
     print(r.medidas)
 
--- desafios/des031/classe.py
+++ desafios/des031/classe.py
@@ -1,35 +1,47 @@
 class Retangulo:
     def __init__(self, base=1, altura=1):
-        self._base = base
-        self._altura = altura
+        self._base = None
+        self._altura = None
         self._area = None
+        self.base = base
+        self.altura = altura
 
     @property
     def altura(self):
         return self._altura
 
     @altura.setter
-    def altura(self, a):
-        if a < 0:
-            raise ValueError('Valor para altura inválido!')
+    def altura(self, valor):
+        if isinstance(valor, float) or isinstance(valor, int):
+            if valor < 1:
+                raise ValueError('Valor para altura inválido!')
+            else:
+                self._altura = valor
         else:
-            self._altura = a
+            raise ValueError('O valor da altura deve ser um número')
 
     @property
     def base(self):
         return self._base
 
     @base.setter
-    def base(self, b):
-        if b < 0:
-            raise ValueError('Valor para base inválido!')
+    def base(self, valor):
+        if isinstance(valor, float) or isinstance(valor, int):
+            if valor < 1:
+                raise ValueError('Valor para base inválido!')
+            else:
+                self._base = valor
         else:
-            self._base = b
+            raise ValueError('O valor da base deve ser um número')
 
     @property
     def area(self):
         self._area = self.base * self.altura
         return self._area
+
+    @area.setter
+    def area(self):
+        raise PermissionError('Você não pode mexer na área.')
 
     @property
     def medidas(self):
@@ -37,12 +49,18 @@ class Retangulo:
 
     @medidas.setter
     def medidas(self, med):
-        for i, n in enumerate(med):
-            if n < 0:
-                raise ValueError('Medidas inválidas!')
-            elif i == 0:
-                self.base = n
-            elif i == 1:
-                self.altura = n
-            else:
-                raise ValueError('Capotemo o corsa!')
+        if not isinstance(med, tuple):
+            raise ValueError('Os valores de medida devem ser informados dentro de uma tupla!')
+        elif len(med) != 2:
+            raise ValueError('A tupla deve conter apenas duas medidas (base, altura)')
+        else:
+            for i, n in enumerate(med):
+                if n < 0 or isinstance(n, str):
+                    raise ValueError('Medidas inválidas!')
+                elif i == 0:
+                    self.base = n
+                elif i == 1:
+                    self.altura = n
+                else:
+                    raise ValueError('Capotemo o corsa!')
+
